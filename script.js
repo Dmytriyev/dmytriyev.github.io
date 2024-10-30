924364a7a38fe9d0767755c4ba6437ed2bf2d1ed
Dmytriyev <dmytriyevapple@gmail.com>
2024-10-30 16:11:54 +0100

    final

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 var newWindow = null;
 var PreviusUrl;
-function openNewWindow(url) {
+function openNewWindowPopup(url) {
     if (newWindow == null || newWindow.closed) {
-        newWindow = window.open(url, "openNewWindow");
+        newWindow = window.open(url, "openNewWindow",);
     }
+    else if (PreviusUrl != url) {
+        newWindow = window.open(url, "openNewWindow",);
+        newWindow.focus();
+    }
+    else {
+        newWindow.focus();
+    }
+    PreviusUrl = url;
 }
